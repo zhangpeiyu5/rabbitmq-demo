451726bbf58dd09206a8e7655a408165187a873f
zhangpeiyu <zhangpeiyu4@jd.com>
2019-12-25 09:26:42 +0800

    rabbitmq-demo 初始化

--- src/main/java/com/zpy/rabbitmq/RabbitmqDemoApplication.java
+++ src/main/java/com/zpy/rabbitmq/RabbitmqDemoApplication.java
@@ -2,12 +2,15 @@ package com.zpy.rabbitmq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
+@ImportResource(locations = {"classpath:beans.xml"})
+@PropertySource(value = {"classpath:testzpy.yml"})
 @SpringBootApplication
 public class RabbitmqDemoApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(RabbitmqDemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqDemoApplication.class, args);
+    }
 
 }
